c50e5530046584a0dbf0ba6be6e57e6cbec79bb4
RRRR8 <166377229+RRRR8@users.noreply.github.com>
2024-04-08 18:50:09 +1000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+function perelet(){
+	document.getElementById("three").value=document.getElementById("one").innerText;
+	document.getElementById("four").value==document.getElementById("two").innerText;
+}
+function vnimanie(){
+	alert("Спасибо за заказ");
+}
